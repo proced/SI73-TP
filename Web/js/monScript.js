4257957136545ffce0908c10441fcf3776b761fb
Ahmet01100 <ahmet.imre@hotmail.fr>
2016-06-09 08:52:50 +0200

    Correction du bug concernant le bouton ajouter et vérification des champs avant l'ajout

--- Web/js/monScript.js
+++ Web/js/monScript.js
@@ -3,11 +3,18 @@ function ajoutParticipant()
 	var nom=document.getElementById('nom').value;
 	var prenom=document.getElementById('prenom').value;
 	var filiere=document.getElementById('filiere').value;
+    
+    
+    if(nom == '' || prenom == '' || filiere == '')
+        alert("Vous devez saisir toutes les informations pour ajouter le participant");
+	else
+    {
+        maDiv = document.createElement("div");
+        maDiv.innerHTML = '<div class="alert alert-info alert-dismissable fade in"> <div class="pull-left"><img width="40" src="img/profile-pics/5.jpg" alt=""></div><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><small class="text-muted">'+nom+' '+prenom+'</small><br><p>'+filiere+'</p></div>'; 
+
+        document.getElementById('divParticipants').appendChild(maDiv);
+    }
 	
-	maDiv = document.createElement("div");
-	maDiv.innerHTML = '<div class="alert alert-info alert-dismissable fade in"> <div class="pull-left"><img width="40" src="img/profile-pics/5.jpg" alt=""></div><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><small class="text-muted">'+nom+' '+prenom+'</small><br><p>'+filiere+'</p></div>'; 
-	
-    document.getElementById('divParticipants').appendChild(maDiv);
 
 }
 
